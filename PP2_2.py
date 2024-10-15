a0e1467739bb2c0fdc001f730a32b00a3a2e4356
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-10-15 14:40:44 +0000

    hfh

--- PP2_2.py
+++ PP2_2.py
@@ -2,17 +2,33 @@
 
 def q1(): 
   #Write Assignment code here
-
+  number=eval(input("Input an integer: "))
+  if number==5:
+    print("The number is Five")
+  else:
+    print("The number is not Five")
 
 def q2(): 
   #Write Assignment code here
-
+  number=eval(input("Input a number: "))
+  if number>0:
+    print("Positive")
+  else:
+    print("Negative")
 def q3(): 
   #Write Assignment code here
-
+  number=eval(input("Input an integer: "))
+  if number%2==0:
+    print("Even")
+  else:
+    print("Odd")
 def q4(): 
   #Write Assignment code here
-
+ word=input('Type "Hello": ')
+ if word=="Hello":
+  print("The word is Hello")
+ else:
+  print("The word is not Hello")
 
 
 
