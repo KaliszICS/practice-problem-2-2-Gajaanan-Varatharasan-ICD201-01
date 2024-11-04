326a1056981088fda4a3f28b04681853480f9c79
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-11-04 10:44:34 -0500

    Update PP2_2.py

--- PP2_2.py
+++ PP2_2.py
@@ -3,29 +3,29 @@
 def q1(): 
   #Write Assignment code here
   number=int(input("Input an integer: "))
-  if number==5:
+  if number == 5:
     print("The number is Five")
   else:
     print("The number is not Five")
 
 def q2(): 
   #Write Assignment code here
-  number=float(input("Input a number: "))
-  if number>0:
+  number = float(input("Input a number: "))
+  if number >= 1:
     print("Positive")
   else:
     print("Negative")
 def q3(): 
   #Write Assignment code here
-  number=int(input("Input an integer: "))
-  if number%2==0:
+  number = int(input("Input an integer: "))
+  if number % 2 == 0:
     print("Even")
   else:
     print("Odd")
 def q4(): 
   #Write Assignment code here
- word=int('Type "Hello": ')
- if word=="Hello":
+ word = input('Type "Hello": ')
+ if word == "Hello":
   print("The word is Hello")
  else:
   print("The word is not Hello")
